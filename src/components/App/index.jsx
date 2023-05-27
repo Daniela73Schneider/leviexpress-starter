6bfe0a9522ff7a84363792c2fc590f77cf842c25
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-05-27 11:05:27 +0200

    routovani prvni cviceni

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Reservation } from '../Reservation';
 import { Footer } from '../Footer';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+  Router,
+} from 'react-router-dom';
 import { Header } from '../Header';
 import { Home } from '../Home';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/reservation', element: <Reservation /> },
+]);
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={router} />
     <Footer />
   </>
 );
